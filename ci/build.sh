12c826593789de1bf05948ef09d92dea9e1a7499
Alexander.Golubov <Alexander.Golubov@solera.com>
2018-01-25 01:26:20 +0200

    edit

--- ci/build.sh
+++ ci/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir nupkg-app/nuget-task-output
+mkdir nuget-task-output
 cd nupkg-app
-rm -f nuget-task-output/*.nupkg
-dotnet pack -o nuget-task-output/
+rm -f ../nuget-task-output/*.nupkg
+dotnet pack -o ../nuget-task-output/
